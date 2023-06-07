51a240a4435ff199800487483433a31e27316a3a
Oph3o <ooph3o@gmail.com>
2023-06-07 16:33:15 -0300

    função onclick para informações aparecerem

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
+    function clicar(){
+        let nome = document.getElementById('nome').value;
+        document.getElementById('name').innerHTML = nome
+
+        var email = document.getElementById('email').value;
+        document.getElementById('mail').innerHTML = email
+
+        var number = document.getElementById('numero').value;
+        document.getElementById('telefone').innerHTML = number
+
+        var endereco = document.getElementById('endereco').value;
+        document.getElementById('localizacao').innerHTML = endereco
+    }
+
 function empresa() {
     var empresas = document.querySelectorAll('.emp')
-
+    let imagem = document.querySelectorAll('.img')
         if (empresas[0].selected) {
-            let imagem = document.querySelectorAll('.img')
             imagem[0].style.display = "block";
             imagem[1].style.display = "none";
             imagem[2].style.display = "none";
             imagem[3].style.display = "none";
 
         } else if (empresas[1].selected) {
-            let imagem = document.querySelectorAll('.img')
             imagem[1].style.display = "block";
             imagem[0].style.display = "none";
             imagem[2].style.display = "none";
             imagem[3].style.display = "none";
             
         } else if (empresas[2].selected) {
-            let imagem = document.querySelectorAll('.img')
             imagem[2].style.display = "block";
             imagem[0].style.display = "none";
             imagem[1].style.display = "none";
             imagem[3].style.display = "none";
 
         } else if (empresas[3].selected) {
-            let imagem = document.querySelectorAll('.img')
             imagem[3].style.display = "block";
             imagem[0].style.display = "none";
             imagem[1].style.display = "none";
             imagem[2].style.display = "none";
         } 
 }
-
-    var nome = document.getElementById('nome');
-    var email = document.getElementById('email');
-    var number = document.getElementById('numero');
-    var endereco = document.getElementById('endereco');

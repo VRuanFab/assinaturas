c16b0ab7d9768930e252182d20ede58e7b1da934
VRuanFab <135835688+VRuanFab@users.noreply.github.com>
2023-06-09 23:48:34 -0300

    Agro quase completa

--- main.js
+++ main.js
@@ -9,8 +9,8 @@
                     imagem[2].style.display = "none";
                     imagem[3].style.display = "none";
                     imagem[4].style.display = "none";
-                    info(0)
-                    localidade()
+                    info(0);
+                    localidade();
         
                 } else if (empresas[1].selected) {
                     //daf
@@ -19,8 +19,8 @@
                     imagem[2].style.display = "none";
                     imagem[3].style.display = "none";
                     imagem[4].style.display = "none";
-                    info(1)
-                    localidade()
+                    info(1);
+                    localidade();
                     
                 } else if (empresas[2].selected) {
                     //asf
@@ -29,8 +29,8 @@
                     imagem[1].style.display = "none";
                     imagem[3].style.display = "none";
                     imagem[4].style.display = "none";
-                    info(2)
-                    localidade()
+                    info(2);
+                    localidade();
         
                 } else if (empresas[3].selected) {
                     //eng
@@ -39,8 +39,8 @@
                     imagem[1].style.display = "none";
                     imagem[2].style.display = "none";
                     imagem[4].style.display = "none";
-                    info(3)
-                    localidade()
+                    info(3);
+                    localidade();
 
                 } else if (empresas[4].selected) {
                     //pav
@@ -49,11 +49,11 @@
                     imagem[1].style.display = "none";
                     imagem[2].style.display = "none";
                     imagem[3].style.display = "none";
-                    info(4)
-                    localidade()
+                    info(4);
+                    localidade();
                 }
         }
-        empresa()
+        empresa();
 
         function info(n) {
             if (n == 0) {
@@ -68,9 +68,7 @@
     
                     var number = document.getElementById('numero').value;
                     document.getElementById('telefoneagr').innerHTML = number
-    
-                    /*var endereco = document.getElementById('endereco').value;
-                    document.getElementById('localizacaoagr').innerHTML = endereco*/
+
             } else if (n == 1) {
                     var nome = document.getElementById('nome').value;
                     document.getElementById('namedaf').innerHTML = nome
@@ -84,8 +82,6 @@
                     var number = document.getElementById('numero').value;
                     document.getElementById('telefonedaf').innerHTML = number
         
-                    var endereco = document.getElementById('endereco').value;
-                    document.getElementById('localizacaodaf').innerHTML = endereco
             } else if (n == 2) {
                     var nome = document.getElementById('nome').value;
                     document.getElementById('nameasf').innerHTML = nome
@@ -99,8 +95,6 @@
                     var number = document.getElementById('numero').value;
                     document.getElementById('telefoneasf').innerHTML = number
         
-                    var endereco = document.getElementById('endereco').value;
-                    document.getElementById('localizacaoasf').innerHTML = endereco
             } else if (n == 3) {
                     var nome = document.getElementById('nome').value;
                     document.getElementById('nameeng').innerHTML = nome
@@ -114,8 +108,6 @@
                     var number = document.getElementById('numero').value;
                     document.getElementById('telefoneeng').innerHTML = number
                 
-                    var endereco = document.getElementById('endereco').value;
-                    document.getElementById('localizacaoeng').innerHTML = endereco
             } else if (n == 4) {
                     var nome = document.getElementById('nome').value;
                     document.getElementById('namepav').innerHTML = nome
@@ -129,26 +121,59 @@
                     var number = document.getElementById('numero').value;
                     document.getElementById('telefonepav').innerHTML = number
         
-                    var endereco = document.getElementById('endereco').value;
-                    document.getElementById('localizacaopav').innerHTML = endereco
                 }
             } //função que vai chamar as informações de estilos diferentes
 
         function localidade() {
             var localizacao = document.querySelectorAll('.location')
-            if (localizacao[0]) {
-                enderecos(0)
-        }   else if (localizacao[1]) {
-                
+            if (localizacao[0].selected) {
+                enderecos(0);
+        }   else if (localizacao[1].selected) {
+                enderecos(1)
+        }   else if (localizacao[2].selected) {
+                enderecos(2);
+        } else if (localizacao[3].selected) {
+                enderecos(3);
+        } else if (localizacao[4].selected) {
+                enderecos(4);
         }
         }
-    }
-
-    function enderecos(base) {
-        if (base == 0)
-        document.getElementById('localizacaoagr').innerHTML = "Rua Serpa, 255-5 - Cajazeiras, Fortaleza - CE, 60864-440"
-        document.getElementById('localizacaodaf').innerHTML = "Rua Serpa, 255-5 - Cajazeiras, Fortaleza - CE, 60864-440"
-        document.getElementById('localizacaoasf').innerHTML = "Rua Serpa, 255-5 - Cajazeiras, Fortaleza - CE, 60864-440"
-        document.getElementById('localizacaoeng').innerHTML = "Rua Serpa, 255-5 - Cajazeiras, Fortaleza - CE, 60864-440"
-        document.getElementById('localizacaopav').innerHTML = "Rua Serpa, 255-5 - Cajazeiras, Fortaleza - CE, 60864-440"
+        function enderecos(base) {
+            if (base == 0) {
+            document.getElementById('localizacaoagr').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('cep').innerHTML = "60864-440"
+            document.getElementById('localizacaodaf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('localizacaoasf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('localizacaoeng').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('localizacaopav').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+        }   else if(base == 1) {
+            document.getElementById('localizacaoagr').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('cep').innerHTML = "65095-601"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+        } else if(base == 2) {
+            document.getElementById('localizacaoagr').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('cep').innerHTML = "59149-070"
+            document.getElementById('localizacaodaf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('localizacaoasf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('localizacaoeng').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('localizacaopav').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+        } else if(base == 3) {
+            document.getElementById('localizacaoagr').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('cep').innerHTML = "64022-800"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+        } else if(base == 4) {
+            document.getElementById('localizacaoagr').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('cep').innerHTML = "64900-000"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+        }
+        }
     }

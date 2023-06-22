6da63ae6fc908628787fcf8a277e245068b6fec5
VRuanFab <victor.ruanfab@gmail.com>
2023-06-22 01:39:13 -0300

    projeto praticamente finalizado

--- exemplo.js
+++
@@ -1,4 +0,0 @@
-const f2 = "BEGIN:VCARD\nVERSION:3.0\nN:Ruan Fabricio\nORG:Fornecedora\nTEL:85989387167\nEMAIL:victor.cavalcante@grupofornecedora.com.br\nEND:VCARD"
-const encoded = encodeURIComponent(f2);
-var link = "https://quickchart.io/qr?text="  + encoded
-console.log(link)
--- qrcode.js
+++
@@ -1,6 +0,0 @@
-
-function clicar() {
-    var inputnome = document.getElementById('nome');
-    var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${inputnome}`
-    document.getElementById('QRCodeImage').src = GoogleChartAPI;
-}

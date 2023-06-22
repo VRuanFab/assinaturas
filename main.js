5b7e1365a0f708f13f944b101d67ba58ffdbd68f
VRuanFab <victor.ruanfab@gmail.com>
2023-06-22 01:38:33 -0300

    projeto praticamente finalizado

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-window.addEventListener('aberto', clicar())
     function clicar() {
         //função do onchange principal das empresas
         function empresa() {
@@ -15,8 +14,8 @@ window.addEventListener('aberto', clicar())
                     imagem[5].style.display = "none";
                     info(0);
                     localidade();
-                    desbloquear()
-                    formato();
+                    desbloquear();
+                    qrcode();
         
                 } else if (empresas[1].selected) {
                     //agro
@@ -29,7 +28,6 @@ window.addEventListener('aberto', clicar())
                     info(1);
                     localidade();
                     bloquear();
-                    formato();
         
                 } else if (empresas[2].selected) {
                     //daf
@@ -100,16 +98,12 @@ window.addEventListener('aberto', clicar())
                 number = number.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
                 document.getElementById('telefonegrp').innerHTML = number
 
-                
-                if (nome.value === ""){
+                    var f2 = `BEGIN:VCARD\nVERSION:3.0\nN:${nome}\nTEL:${number}\nEMAIL:${email}@grupofornecedora.com.br\nEND:VCARD`
+                    const encoded = encodeURIComponent(f2);
+                    var link = "https://quickchart.io/qr?text="  + encoded
+                    document.getElementById('QRCodeImagegrp').src = link
                     
-                }else {
-                    var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${nome}${email}${number}`
-                document.getElementById('QRCodeImage').src = GoogleChartAPI;
-                }
-
-            } 
-            if (n == 1) {
+            } else if (n == 1) {
                 var nome = document.getElementById('nome').value;
                 document.getElementById('nameagr').innerHTML = nome
 
@@ -134,8 +128,10 @@ window.addEventListener('aberto', clicar())
                 number = number.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
                 document.getElementById('telefonedaf').innerHTML = number
 
-                var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${nome}${email}${number}`
-                document.getElementById('QRCodeImage').src = GoogleChartAPI;
+                    var f2 = `BEGIN:VCARD\nVERSION:3.0\nN:${nome}\nTEL:${number}\nEMAIL:${email}@fornecedoracaminhoes.com.br\nEND:VCARD`
+                    const encoded = encodeURIComponent(f2);
+                    var link = "https://quickchart.io/qr?text="  + encoded
+                    document.getElementById('QRCodeImagedaf').src = link
         
             } else if (n == 3) {
                     var nome = document.getElementById('nome').value;
@@ -151,8 +147,10 @@ window.addEventListener('aberto', clicar())
                     number = number.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
                     document.getElementById('telefoneasf').innerHTML = number
 
-                    var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${nome}${email}${number}`
-                document.getElementById('QRCodeImage').src = GoogleChartAPI;
+                        var f2 = `BEGIN:VCARD\nVERSION:3.0\nN:${nome}\nTEL:${number}\nEMAIL:${email}@fornecedoraasfaltos.com.br\nEND:VCARD`
+                        const encoded = encodeURIComponent(f2);
+                        var link = "https://quickchart.io/qr?text="  + encoded
+                        document.getElementById('QRCodeImageasf').src = link
         
             } else if (n == 4) {
                     var nome = document.getElementById('nome').value;
@@ -167,9 +165,11 @@ window.addEventListener('aberto', clicar())
                     var number = document.getElementById('numero').value;
                     number = number.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
                     document.getElementById('telefoneeng').innerHTML = number
-
-                    var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${nome}${email}${number}`
-                    document.getElementById('QRCodeImage').src = GoogleChartAPI;
+                    
+                        var f2 = `BEGIN:VCARD\nVERSION:3.0\nN:${nome}\nTEL:${number}\nEMAIL:${email}@fornecedoraengelog.com.br\nEND:VCARD`
+                        const encoded = encodeURIComponent(f2);
+                        var link = "https://quickchart.io/qr?text="  + encoded
+                        document.getElementById('QRCodeImageeng').src = link
                 
             } else if (n == 5) {
                     var nome = document.getElementById('nome').value;
@@ -185,9 +185,12 @@ window.addEventListener('aberto', clicar())
                     number = number.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
                     document.getElementById('telefonepav').innerHTML = number
 
-                    var GoogleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${nome}${email}${number}`
-                    document.getElementById('QRCodeImage').src = GoogleChartAPI;
+                        var f2 = `BEGIN:VCARD\nVERSION:3.0\nN:${nome}\nTEL:${number}\nEMAIL:${email}@pavimentos.com.br\nEND:VCARD`
+                        const encoded = encodeURIComponent(f2);
+                        var link = "https://quickchart.io/qr?text="  + encoded
+                        document.getElementById('QRCodeImagepav').src = link
                 }
+
             }
 
         //exibe as localidades também na função de onchange
@@ -211,48 +214,48 @@ window.addEventListener('aberto', clicar())
         //descreve qual empresa é para a função localidade
         function enderecos(base) {
             if (base == 0) {
-            document.getElementById('localizacaogrp').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('localizacaogrp').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE, 60864-440"
             document.getElementById('localizacaoagr').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
             document.getElementById('cep').innerHTML = "60864-440"
-            document.getElementById('localizacaodaf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
-            document.getElementById('localizacaoasf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
-            document.getElementById('localizacaoeng').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
-            document.getElementById('localizacaopav').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE"
+            document.getElementById('localizacaodaf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE, 60864-440"
+            document.getElementById('localizacaoasf').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE, 60864-440"
+            document.getElementById('localizacaoeng').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE, 60864-440"
+            document.getElementById('localizacaopav').innerHTML = "Rodovia BR 116, 2524 - Km 65 - Cajazeiras, Fortaleza - CE, 60864-440"
         }   else if(base == 1) {
-            document.getElementById('localizacaogrp').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('localizacaogrp').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA, 65095-601"
             document.getElementById('localizacaoagr').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
             document.getElementById('cep').innerHTML = "65095-601"
-            document.getElementById('localizacaodaf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
-            document.getElementById('localizacaoasf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
-            document.getElementById('localizacaoeng').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
-            document.getElementById('localizacaopav').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA, 65095-601"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA, 65095-601"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA, 65095-601"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Engenheiro Emiliano Macieira, 100 - Tibiri, São Luís - MA, 65095-601"
         }   else if(base == 2) {
-            document.getElementById('localizacaogrp').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('localizacaogrp').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN, 59149-070"
             document.getElementById('localizacaoagr').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
             document.getElementById('cep').innerHTML = "59149-070"
-            document.getElementById('localizacaodaf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
-            document.getElementById('localizacaoasf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
-            document.getElementById('localizacaoeng').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
-            document.getElementById('localizacaopav').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN"
+            document.getElementById('localizacaodaf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN, 59149-070"
+            document.getElementById('localizacaoasf').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN, 59149-070"
+            document.getElementById('localizacaoeng').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN, 59149-070"
+            document.getElementById('localizacaopav').innerHTML = "Rua Dom Nivaldo Monte, 103 - Emaús, Parnamirim - RN, 59149-070"
         }   else if(base == 3) {
-            document.getElementById('localizacaogrp').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('localizacaogrp').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI, 64022-800"
             document.getElementById('localizacaoagr').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
             document.getElementById('cep').innerHTML = "64022-800"
-            document.getElementById('localizacaodaf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
-            document.getElementById('localizacaoasf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
-            document.getElementById('localizacaoeng').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
-            document.getElementById('localizacaopav').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI, 64022-800"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI, 64022-800"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI, 64022-800"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Prefeito Wall Ferraz, 9247 - Lourival Parente, Teresina - PI, 64022-800"
         }   else if(base == 4) {
-            document.getElementById('localizacaogrp').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('localizacaogrp').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI, 64900-000"
             document.getElementById('localizacaoagr').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
             document.getElementById('cep').innerHTML = "64900-000"
-            document.getElementById('localizacaodaf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
-            document.getElementById('localizacaoasf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
-            document.getElementById('localizacaoeng').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
-            document.getElementById('localizacaopav').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI"
+            document.getElementById('localizacaodaf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI, 64900-000"
+            document.getElementById('localizacaoasf').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI, 64900-000"
+            document.getElementById('localizacaoeng').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI, 64900-000"
+            document.getElementById('localizacaopav').innerHTML = "Avenida Ademar Diogenes, 583 - BR 135, KM 349 - São Pedro, Bom Jesus - PI, 64900-000"
         }
     }
-    }
+}
 
     //bloqueia o email da assinatura agro (na qual não usa email)
     function bloquear() {
@@ -263,4 +266,5 @@ window.addEventListener('aberto', clicar())
     function desbloquear() {
         let emailbloq = document.getElementById('email')
             emailbloq.disabled = false;
+    
     }
